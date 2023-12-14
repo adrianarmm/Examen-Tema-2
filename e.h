c16a680b3c477193f1958d41166bdf6fbc16d408
adrianarmm <areyemor@myuax.com>
2023-12-14 12:32:10 +0100

    22

--- e.h
+++ e.h
@@ -10,25 +10,4 @@
 #include <stdio.h>
 #include <string.h>
 
-struct Estudiante {
-    char nombre[50];
-    int edad;
-    float promedio;
-};
-
-void mostrarEstudiante(struct Estudiante estudiante) {
-    printf("Nombre: %s\n", estudiante.nombre);
-    printf("Edad: %d\n", estudiante.edad);
-    printf("Promedio: %.2f\n", estudiante.promedio);
-}
-
-
-int main() {
-    struct Estudiante estudiante1;
-    strcpy(estudiante1.nombre, "Adriana");
-    estudiante1.edad = 18;
-    estudiante1.promedio = 9.5;
-    mostrarEstudiante(estudiante1);
-
-    return 0;
-}
+#define GENERIC_TYPE int
