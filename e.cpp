1fba694cdb36b7d1bdd0db3b14074bc5a7c647fd
adrianarmm <areyemor@myuax.com>
2023-12-14 12:19:08 +0100

    6

--- e.cpp
+++ e.cpp
@@ -9,3 +9,8 @@ struct Estudiante {
     };
 
 template <typename A>
+void imprimir(Estudiante<A> estudiante) {
+    std::cout << "Nombre: " << estudiante.nombre << std::endl;
+    std::cout << "Edad: " << estudiante.edad << std::endl;
+    std::cout << "Promedio: " << estudiante.promedio << std::endl;
+}
