54a8a65724ed80994980f5251466658c024c392d
adrianarmm <areyemor@myuax.com>
2023-12-14 13:05:30 +0100

    106

--- o.cpp
+++ o.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
 #include <string>
 #include <stdexcept>
+
+class FormatoFechaInvalido : public std::exception {
+public:
+    const char* what() const noexcept override {
+            return "Error: Formato de fecha inválido.";
+    }
+};
