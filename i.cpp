96a082794160e3d133af3aa138843d2587e60118
adrianarmm <areyemor@myuax.com>
2023-12-14 12:52:31 +0100

    74

---
+++ i.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Adriana Reyes Morera on 14/12/23.
+//
+
+#include "i.h"
---
+++ i.h
@@ -0,0 +1,10 @@
+//
+// Created by Adriana Reyes Morera on 14/12/23.
+//
+
+#ifndef EXAMEN_TEMA_2_I_H
+#define EXAMEN_TEMA_2_I_H
+
+
+
+#endif //EXAMEN_TEMA_2_I_H
