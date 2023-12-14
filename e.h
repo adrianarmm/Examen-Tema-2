7194b6e8fd31fd08df8cc116f21c0ebb18a44504
adrianarmm <areyemor@myuax.com>
2023-12-14 12:39:07 +0100

    43

--- e.h
+++ e.h
@@ -8,26 +8,5 @@
 
 
 #include <stdio.h>
+#include <string.h>
 
-
-#define GENERIC_TYPE int
-#define Estudiante(x) x ## _Estudiante
-#define mostrarEstudiante(x) x ## _mostrarEstudiante
-
-typedef struct Estudiante(GENERIC_TYPE) {
-  char nombre [100];
-  GENERIC_TYPE edad;
-  GENERIC_TYPE promedio;
-}
-Estudiante(GENERIC_TYPE);
-
-void mostrarEstudiante(Estudiante(GENERIC_TYPE) estudiante) {
-  printf("Nombre: %s\n", estudiante.nombre);
-  printf("Edad: %d\n", estudiante.edad);
-  printf("Promedio: %.2f\n", estudiante.promedio);
-}
-
-#undef GENERIC_TYPE
-#undef Estudiante
-#undef mostrarEstudiante
-}
